6efc7b83c719b0febbfca83dba5438df133b45ee
jhlee7854 <jhlee7854@me.com>
2024-05-02 14:14:38 +0900

    Modify greeting controller

--- src/main/java/kr/pe/jonghak/greeting/api/GreetingController.java
+++ src/main/java/kr/pe/jonghak/greeting/api/GreetingController.java
@@ -1,5 +1,6 @@
 package kr.pe.jonghak.greeting.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,14 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class GreetingController {
     @GetMapping("/greet")
     public ResponseEntity<List<String>> greet() {
         RestTemplate restTemplate = new RestTemplate();
-        List<String> users = restTemplate.getForObject("http://localhost:8080/users", List.class);
+        List<String> users = restTemplate.getForObject("http://user-api.user/users", List.class);
         List<String> greetings = users.stream().map(user -> "Hello, " + user).toList();
+        log.info("response greeting list: {}", greetings);
         return ResponseEntity.ok(greetings);
     }
 }
